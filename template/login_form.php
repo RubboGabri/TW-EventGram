4a3421be7b7145bb4e5d381d5f5068f220858b7a
Gabriele Rubboli Petroselli <gabriele.rubboli@studio.unibo.it>
2024-06-21 16:36:09 +0200

    added new branch

--- template/login_form.php
+++ template/login_form.php
@@ -7,18 +7,17 @@
             <p class="text-danger"></p>
             <div class="form-group mb-3">
                 <label for="username" class="fs-6 fw-semibold d-block text-start">Username:</label>
-                <input type="text" id="username" name="username" placeholder="Username" class="form-control">
+                <input type="text" id="username" name="username" placeholder="Username" class="form-control"/>
             </div>
             <div class="form-group mb-3">
                 <label for="password" class="fs-6 fw-semibold d-block text-start">Password:</label>
-                <input type="password" id="password" name="password" placeholder="Password" class="form-control">
+                <input type="password" id="password" name="password" placeholder="Password" class="form-control"/>
             </div>
             <div class="form-group mb-3">
-                <input type="submit" name="submit" value="Accedi" class="btn btn-custom w-100">
+                <input type="submit" name="submit" value="Accedi" class="btn btn-custom w-100"/>
             </div>
         </form>
         <section class="mt-5">
-        <h2 class="d-none">Login Section</h2>
             <span>Non hai un account? </span>
             <a href="../register.php" class="link-primary">Registrati</a>
         </section>
