c7dd28b6b2bbdf90197ed5dd41a9c038eb1efc1e
Gabriele Rubboli Petroselli <gabriele.rubboli@studio.unibo.it>
2024-06-21 17:08:44 +0200

    deleted wrong file

--- user_profile.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("bootstrap.php");
-$templateParams["navbar"] = "template/navbar.html";
-$templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js");
-$templateParams["title"] = $_SESSION['username'] . " - EventGram";
-require_once("template/base.php");
-
-?>
