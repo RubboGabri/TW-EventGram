9201f26e053b04d03cc81894a830981ce99c5be7
MarcoRavaioli <marco.ravaioli8@studio.unibo.it>
2024-06-21 12:08:31 +0200

    solved minor issues with accessibility

--- template/registration_form.php
+++ template/registration_form.php
@@ -7,22 +7,23 @@
                 <p class="text-danger"></p>
                 <div class="form-group mb-3">
                     <label for="username" class="fs-6 fw-semibold d-block text-start">Username:</label>
-                    <input type="text" id="username" name="username" placeholder="Username" class="form-control"/>
+                    <input type="text" id="username" name="username" placeholder="Username" class="form-control">
                 </div>
                 <div class="form-group mb-3">
                     <label for="password" class="fs-6 fw-semibold d-block text-start">Password:</label>
-                    <input type="password" id="password" name="password" placeholder="Password" class="form-control"/>
+                    <input type="password" id="password" name="password" placeholder="Password" class="form-control">
                 </div>
                 <div class="form-group mb-3">
                     <label for="confirm_password" class="fs-6 fw-semibold d-block text-start">Conferma Password:</label>
-                    <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirm Password" class="form-control"/>
+                    <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirm Password" class="form-control">
                 </div>
                 <div class="form-group mb-3">
-                    <input type="submit" name="submit" value="Registrati" class="btn btn-custom w-100"/>
+                    <input type="submit" name="submit" value="Registrati" class="btn btn-custom w-100">
                 </div>
             </form>
             <section class="mt-5">
                 <span>Hai già un account? </span>
+                <h2 class="d-none">Registration Section</h2>
                 <a href="index.php" class="link-primary">Accedi</a>
             </section> 
         </div>
