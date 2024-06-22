034eaff67fca137a84ce3cd1000438bac7ce739b
MarcoRavaioli <marco.ravaioli8@studio.unibo.it>
2024-06-22 17:14:33 +0200

    correcter the api.pgp pattern

--- notification.php
+++ notification.php
@@ -1,8 +1,36 @@
 <?php
-require_once 'bootstrap.php';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once("bootstrap.php");
+
+// Imposta i template
 $templateParams["navbar"] = "template/navbar.html";
-$templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js", "js/notification.js");
-$templateParams["title"] = "Notifiche - EventGram";
+$templateParams["js"] = array("https://unpkg.com/axios/dist/axios.min.js", "js/logout.js");
 $templateParams["main"] = "template/notification_list.php";
-require_once 'template/base.php';
+
+// Ottieni l'ID utente loggato
+if (isset($_SESSION["idUser"])) {
+    $templateParams["user"] = $_SESSION["idUser"];
+    echo "Logged User ID: " . $templateParams["user"] . "<br>";  // Debugging
+} else {
+    header("Location: login.php");
+    exit;
+}
+
+// Recupera le notifiche per l'utente loggato
+$notifications = $dbh->getNotifications($templateParams["user"]);
+foreach ($notifications as &$notification) {
+    $userInfo = $dbh->getUserById($notification['notifier']);
+    $user = $userInfo[0];
+    $notification['notifier_username'] = $user['username'];
+    $notification['notifier_pic'] = $user['profilePic'];
+}
+
+$templateParams["title"] = "Notifiche - EventGram";
+$templateParams["notifications"] = $notifications;
+
+require_once("template/base.php");
 ?>
--- template/notification_list.php
+++ template/notification_list.php
@@ -1,6 +1,51 @@
 <div class="container min-vh-100 d-flex flex-column ps-4">
     <h1>Notifiche</h1>
-    <ul id="notifications" class="list-group">
-        <!-- Notifications will be populated here by JavaScript -->
+    <ul class="list-group">
+        <?php if (count($templateParams["notifications"]) > 0): ?>
+            <?php foreach ($templateParams["notifications"] as $notification): ?>
+                <li class="list-group-item d-flex align-items-center">
+                    <!-- Immagine del profilo -->
+                    <img src="img/profile.png" alt="Profile Image" class="img-fluid me-3" style="max-height: 40px;">
+                    <!-- Testo della notifica -->
+                    <span>
+                        <!-- Nome dell'utente -->
+                        <span class="fw-bold text-dark">
+                            <?php echo htmlspecialchars($notification['notifier_username']); ?>
+                        </span>
+                        <!-- Descrizione della notifica -->
+                        <?php
+                            switch ($notification['type']) {
+                                case 'Comment':
+                                    echo " ha commentato il tuo post ";
+                                    if (!empty($notification['IDpost'])) {
+                                        echo "con ID " . htmlspecialchars($notification['IDpost']);
+                                    }
+                                    break;
+                                case 'Like':
+                                    echo " ha messo mi piace al tuo post ";
+                                    if (!empty($notification['IDpost'])) {
+                                        echo "con ID " . htmlspecialchars($notification['IDpost']);
+                                    }
+                                    break;
+                                case 'Follow':
+                                    echo " ha iniziato a seguirti";
+                                    break;
+                                case 'Post':
+                                    echo " ha pubblicato un nuovo post ";
+                                    if (!empty($notification['IDpost'])) {
+                                        echo "con ID " . htmlspecialchars($notification['IDpost']);
+                                    }
+                                    break;
+                                default:
+                                    echo " tipo di notifica sconosciuto";
+                                    break;
+                            }
+                        ?>
+                    </span>
+                </li>
+            <?php endforeach; ?>
+        <?php else: ?>
+            <li class="list-group-item">Nessuna notifica trovata.</li>
+        <?php endif; ?>
     </ul>
 </div>
